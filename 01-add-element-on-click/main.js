e84f827a70b682ebb43f3b3adb83d0d45c3fe891
Skjoeldkrona <skjoeldkrona@gmail.com>
2024-03-19 13:29:59 +0100

    Addding list items on click complted

--- 01-add-element-on-click/main.js
+++ 01-add-element-on-click/main.js
@@ -1,13 +1,23 @@
 const button = document.querySelector('button');
-const body = document.querySelector('body');
 let counter = 0;
 
 button.addEventListener('click', () => {
   const div = document.createElement('div');
   div.textContent = ++counter;
-  body.appendChild(div);
+  document.body.appendChild(div);
 
   if (counter % 5 === 0) {
     div.className = 'circle';
   }
+});
+
+const listBtn = document.querySelector('#list');
+const ul = document.createElement('ul');
+document.body.appendChild(ul);
+let liNumber = 1;
+
+listBtn.addEventListener('click', () => {
+  const li = document.createElement('li');
+  li.textContent = `item no ${liNumber++}`;
+  ul.appendChild(li);
 });
