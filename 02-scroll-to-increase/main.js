672ba02c408c02285611b005fe7c360b3ffa0f37
Skjoeldkrona <skjoeldkrona@gmail.com>
2024-03-20 11:24:44 +0100

    Project Scroll to enlarge square completed

--- 02-scroll-to-increase/main.js
+++ 02-scroll-to-increase/main.js
@@ -1,1 +1,26 @@
-console.log('hello you');
+const square = document.createElement('div');
+document.body.appendChild(square);
+
+let size = 10;
+let grow = true;
+
+square.style.height = size + 'px';
+square.style.width = size + 'px';
+
+window.addEventListener('scroll', () => {
+  if (size >= window.innerWidth / 2) {
+    grow = !grow;
+  } else if (size === 0) {
+    grow = !grow;
+  }
+
+  if (grow) {
+    size++;
+    square.style.height = size + 'px';
+    square.style.width = size + 'px';
+  } else {
+    size--;
+    square.style.height = size + 'px';
+    square.style.width = size + 'px';
+  }
+});
