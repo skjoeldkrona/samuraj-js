ef9092e155bc004a83b23f05de3ec5d9ee1ed382
Skjoeldkrona <skjoeldkrona@gmail.com>
2024-03-21 13:24:25 +0100

    Div increase and change color on scroll completed

--- 02-scroll-to-increase/main.js
+++ 02-scroll-to-increase/main.js
@@ -1,26 +1,59 @@
-const square = document.createElement('div');
-document.body.appendChild(square);
+// const square = document.createElement('div');
+// document.body.appendChild(square);
+
+// let size = 10;
+// let grow = true;
+
+// square.style.height = size + 'px';
+// square.style.width = size + 'px';
+
+// window.addEventListener('scroll', () => {
+//   if (size >= window.innerWidth / 2) {
+//     grow = !grow;
+//   } else if (size === 0) {
+//     grow = !grow;
+//   }
+
+//   if (grow) {
+//     size++;
+//     square.style.height = size + 'px';
+//     square.style.width = size + 'px';
+//   } else {
+//     size--;
+//     square.style.height = size + 'px';
+//     square.style.width = size + 'px';
+//   }
+// });
+
+const divBar = document.body.appendChild(document.createElement('div'));
 
 let size = 10;
 let grow = true;
 
-square.style.height = size + 'px';
-square.style.width = size + 'px';
+document.body.style.height = '10000px';
+divBar.style.position = 'fixed';
+divBar.style.top = 0;
+divBar.style.left = 0;
+divBar.style.height = size + 'px';
+divBar.style.width = '100%';
+divBar.style.backgroundColor = 'red';
 
-window.addEventListener('scroll', () => {
-  if (size >= window.innerWidth / 2) {
+window.addEventListener('scroll', changeHeight);
+
+function changeHeight() {
+  if (size >= window.innerHeight / 2) {
     grow = !grow;
-  } else if (size === 0) {
+    divBar.style.backgroundColor = 'green';
+  } else if (size <= 0) {
     grow = !grow;
+    divBar.style.backgroundColor = 'red';
   }
 
   if (grow) {
     size++;
-    square.style.height = size + 'px';
-    square.style.width = size + 'px';
   } else {
     size--;
-    square.style.height = size + 'px';
-    square.style.width = size + 'px';
   }
-});
+
+  divBar.style.height = size + 'px';
+}
