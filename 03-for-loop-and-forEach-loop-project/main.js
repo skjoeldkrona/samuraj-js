e865166acbadb0509baeac62c3547ae55dd6babe
Skjoeldkrona <skjoeldkrona@gmail.com>
2024-03-23 12:12:33 +0100

    Solution with forEach added

--- 03-for-loop-and-forEach-loop-project/main.js
+++ 03-for-loop-and-forEach-loop-project/main.js
@@ -1,1 +1,11 @@
-console.log('Hello!');
+const btn = document.querySelector('button');
+const list = document.querySelectorAll('ul li');
+let size = 10;
+
+btn.addEventListener('click', () => {
+  list.forEach((li) => {
+    li.style.display = 'block';
+    size++;
+    li.style.fontSize = size + 'px';
+  });
+});
