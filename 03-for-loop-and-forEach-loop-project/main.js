07f77df301968ea37b7dcc45d1121ca9d3197b54
Skjoeldkrona <skjoeldkrona@gmail.com>
2024-03-23 21:54:17 +0100

    Displaying li elements project code refactored

--- 03-for-loop-and-forEach-loop-project/main.js
+++ 03-for-loop-and-forEach-loop-project/main.js
@@ -1,19 +1,21 @@
 const btn = document.querySelector('button');
-const list = document.querySelectorAll('ul li');
+const list = document.querySelectorAll('li');
 let size = 10;
 
-// btn.addEventListener('click', () => {
-//   list.forEach((li) => {
-//     li.style.display = 'block';
-//     li.style.fontSize = size + 'px';
-//   });
-//   size++;
-// });
-
+// My solution with forEach
 btn.addEventListener('click', () => {
-  for (let i = 0; i < list.length; i++) {
-    list[i].style.display = 'block';
-    list[i].style.fontSize = size + 'px';
-  }
+  list.forEach((li) => {
+    li.style.display = 'block';
+    li.style.fontSize = `${size}px`;
+  });
   size++;
 });
+
+// My solution with for loop
+// btn.addEventListener('click', () => {
+//   for (let i = 0; i < list.length; i++) {
+//     list[i].style.display = 'block';
+//     list[i].style.fontSize = size + 'px';
+//   }
+//   size++;
+// });
