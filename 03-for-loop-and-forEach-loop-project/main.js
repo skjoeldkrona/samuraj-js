313e1f3205003d9c0ab500cd0158850d992bf1ac
Skjoeldkrona <skjoeldkrona@gmail.com>
2024-03-24 17:49:04 +0100

    Adding and increasing elements on click completed

--- 03-for-loop-and-forEach-loop-project/main.js
+++ 03-for-loop-and-forEach-loop-project/main.js
@@ -1,15 +1,15 @@
-const btn = document.querySelector('button');
-const list = document.querySelectorAll('li');
-let size = 10;
+// const btn = document.querySelector('button');
+// const list = document.querySelectorAll('li');
+// let size = 10;
 
 // My solution with forEach
-btn.addEventListener('click', () => {
-  list.forEach((li) => {
-    li.style.display = 'block';
-    li.style.fontSize = `${size}px`;
-  });
-  size++;
-});
+// btn.addEventListener('click', () => {
+//   list.forEach((li) => {
+//     li.style.display = 'block';
+//     li.style.fontSize = `${size}px`;
+//   });
+//   size++;
+// });
 
 // My solution with for loop
 // btn.addEventListener('click', () => {
@@ -19,3 +19,40 @@ btn.addEventListener('click', () => {
 //   }
 //   size++;
 // });
+
+let size = 10;
+let liOrder = 1;
+
+const init = () => {
+  const button = document.createElement('button');
+  button.textContent = 'Add 10 elements to the list';
+  button.style.marginRight = '10px';
+  document.body.appendChild(button);
+
+  const resetButton = document.createElement('button');
+  resetButton.textContent = 'Reset';
+  document.body.appendChild(resetButton);
+
+  const ulList = document.createElement('ul');
+  document.body.appendChild(ulList);
+
+  button.addEventListener('click', createLiElems);
+  resetButton.addEventListener('click', resetList);
+};
+
+function createLiElems() {
+  for (let i = 0; i < 10; i++) {
+    const li = document.createElement('li');
+    li.textContent = `item no ${liOrder++}`;
+    li.style.fontSize = `${size++}px`;
+    let ulList = document.querySelector('ul').appendChild(li);
+  }
+}
+
+function resetList() {
+  document.querySelector('ul').innerHTML = '';
+  size = 10;
+  liOrder = 1;
+}
+
+init();
