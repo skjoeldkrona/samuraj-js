fdd35558dc5d9770597573f1813228659a083bbc
Skjoeldkrona <skjoeldkrona@gmail.com>
2024-03-26 18:30:51 +0100

    Feat: changing color with arrow keys

--- 04-events/main.js
+++ 04-events/main.js
@@ -50,36 +50,78 @@
 //   }
 // }
 
-const div = document.querySelector('div');
-let divX = 0;
-let divY = 0;
+// Solution: moving square with mouse
+// const div = document.querySelector('div');
+// let divX = 0;
+// let divY = 0;
 
-div.style.left = `${divX}px`;
-div.style.top = `${divY}px`;
+// div.style.left = `${divX}px`;
+// div.style.top = `${divY}px`;
 
-let insideDivX;
-let insideDivY;
+// let insideDivX;
+// let insideDivY;
 
-let drawActive = false;
+// let drawActive = false;
 
-div.addEventListener('mousedown', (e) => {
-  drawActive = !drawActive;
-  div.style.backgroundColor = 'grey';
-  insideDivX = e.offsetX;
-  insideDivY = e.offsetY;
-});
+// div.addEventListener('mousedown', (e) => {
+//   drawActive = !drawActive;
+//   div.style.backgroundColor = 'grey';
+//   insideDivX = e.offsetX;
+//   insideDivY = e.offsetY;
+// });
 
-div.addEventListener('mousemove', (e) => {
-  if (drawActive) {
-    divX = e.clientX - insideDivX;
-    divY = e.clientY - insideDivY;
+// div.addEventListener('mousemove', (e) => {
+//   if (drawActive) {
+//     divX = e.clientX - insideDivX;
+//     divY = e.clientY - insideDivY;
 
-    div.style.left = `${divX}px`;
-    div.style.top = `${divY}px`;
+//     div.style.left = `${divX}px`;
+//     div.style.top = `${divY}px`;
+//   }
+// });
+
+// div.addEventListener('mouseup', () => {
+//   div.style.backgroundColor = 'orange';
+//   drawActive = !drawActive;
+// });
+
+let red = 100;
+let green = 100;
+let blue = 100;
+
+document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+
+// const changeColor = (e) => {
+//   if (e.keyCode === 38) {
+//     if (red < 255) {
+//       document.body.style.backgroundColor = `rgb(${(red += 5)}, ${(green += 5)}, ${(blue += 5)})`;
+//     }
+//   } else if (e.keyCode === 40) {
+//     if (red > 0)
+//       document.body.style.backgroundColor = `rgb(${(red -= 5)}, ${(green -= 5)}, ${(blue -= 5)})`;
+//   }
+// };
+
+// window.addEventListener('keydown', changeColor);
+
+// Alternative solution with switch
+const changeColor = (e) => {
+  switch (e.keyCode) {
+    case 38:
+      document.body.style.backgroundColor = `rgb(${
+        red < 255 ? (red += 5) : red
+      }, ${green < 255 ? (green += 5) : green}, ${
+        blue < 255 ? (blue += 5) : blue
+      })`;
+      break;
+    case 40:
+      document.body.style.backgroundColor = `rgb(${
+        red > 0 ? (red -= 5) : red
+      }, ${green > 0 ? (green -= 5) : green}, ${
+        blue > 0 ? (blue -= 5) : blue
+      })`;
+      break;
   }
-});
+};
 
-div.addEventListener('mouseup', () => {
-  div.style.backgroundColor = 'orange';
-  drawActive = !drawActive;
-});
+window.addEventListener('keydown', changeColor);
